e2ec4e493d6c00d810ad3be8cbc9dba3ca37acff
Ashwin Bhanage <bhanageashwin28@gmail.com>
2026-02-18 12:16:45 +0530

    feat(constants): define role and booking/payment state enums

---
+++ src/main/java/com/deskmate/constants/BookingStatus.java
@@ -0,0 +1,7 @@
+package com.deskmate.constants;
+
+public enum BookingStatus {
+    CREATED,
+    PAID,
+    CANCELLED
+}
---
+++ src/main/java/com/deskmate/constants/PaymentMode.java
@@ -0,0 +1,7 @@
+package com.deskmate.constants;
+
+public enum PaymentMode {
+    CASH,
+    CARD,
+    UPI
+}
